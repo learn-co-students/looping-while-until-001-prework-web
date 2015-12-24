33598eee6d168c3888cf307f37cf3fccdb902a5c
joeylehr <joe.c.lehr@gmail.com>
2015-12-23 22:35:18 -0500

    noice

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,31 @@
 def using_while
-	levitation_force = 6
-	
-	#your code here
+  levitation_force = 6
+  while levitation_force < 10
+    puts "Wingardium Leviosa"
+    levitation_force += 1
+  end
 end
 
 
+
+
+
+#old code#
+
+#def using_while
+#	levitation_force = 6
+#	loop do
+#    levitation_force +=1
+#    puts "Wingardium Leviosa"
+#    if levitation_force >= 10
+#      break
+#    end
+#  end
+#	#your code here
+#end
+
+
+
+#* Fill out the content of the `using_while` method so that calling it will `puts` the desired phrase 
+#while your levitation force is less than `10`. Remember, every time you `puts` the phrase, 
+#you should increment your levitation force by `1`.
