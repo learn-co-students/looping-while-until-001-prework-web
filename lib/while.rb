ce186e28a09bc792edb88dd1e9e8a736bc272a1a
PatrickSikhrangkur <patrick.sikhrangkur@gmail.com>
2016-01-23 22:14:31 -0500

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,12 @@
 def using_while
-	levitation_force = 6
-	
-	#your code here
-end
+  levitation_force = 6
+	 while levitation_force < 10 do
+	   loop do
+      puts "Wingardium Leviosa\n"
+      levitation_force += 1
+      break if levitation_force == 10
+      end
+    end
+  end
 
-
+  using_while
