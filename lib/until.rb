7acfdfec8b281bb718e456cb909ef5a4c2cc3a78
Joan Manuel Araujo <WISDOMBTHEGAME@GMAIL.COM>
2016-01-28 12:43:34 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,18 @@
 def using_until
-        levitation_force = 6
-        #your code here
-    
+  levitation_force = 6
+until levitation_force == 10
+   puts "Wingardium Leviosa"
+levitation_force += 1
+  end
 end
 
+        #your code here
+
+
+#$i = 0
+#$num = 5
+
+#until $i > $num  do
+   #puts("Inside the loop i = #$i" )
+   #$i +=1;
+#end
