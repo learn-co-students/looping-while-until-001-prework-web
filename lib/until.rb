620020d1567871411d5851ce56b532802e0c1d4f
mjseiman <mjseiman90@gmail.com>
2016-04-11 18:07:05 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,14 +1,10 @@
 def using_until
         levitation_force = 6
-   loop do
-     puts "Wingardium Leviosa"
-     levitation_force += 1
-
-    if levitation_force >=10
-      break
-    end
+   until levitation_force == 10
+		puts "Wingardium Leviosa"
+		levitation_force += 1
+	end
 
 	#your code here
-end
 end
 
--- lib/while.rb
+++ lib/while.rb
@@ -1,14 +1,11 @@
 def using_while
 	levitation_force = 6
-  loop do
-    levitation_force += 1
-      puts "Wingardium Leviosa"
-    if levitation_force >=10
-      break
-    end
+  while levitation_force < 10
+		puts "Wingardium Leviosa"
+		levitation_force += 1
+	end
 
 	#your code here
-end
 end
 
 
