19b6cdb5108ad1566a83611156601dd4d01a2663
juancarlosfield <juancarlosfield@gmail.com>
2016-05-15 09:39:15 -0400

    looping-while-until-001-prework-web

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,20 @@
 def using_until
         levitation_force = 6
         #your code here
+        loop do 
+          levitation_force += 1
+             puts "Wingardium Leviosa"
+             if levitation_force >= 10 
+              break 
+            end 
+          end 
+        end 
+
     
-end
+
+# Let's get the second test passing by coding our solution in until.rb:
+# Fill out the content of the using_until method to puts the desired phrase,
+#  "Wingardium Leviosa", until our levitation force equals 10. Remember, 
+#  every time you puts the phrase, you should increment your levitation 
+#  force by 1.
 
