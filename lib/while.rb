7885c93365bbd5580045af6b420304cd35acc25d
Parmanand Sahadeo <parmsahadeo530@gmail.com>
2016-06-09 20:14:54 +0000

    corrected using while and until loop

--- lib/while.rb
+++ lib/while.rb
@@ -1,13 +1,12 @@
 def using_while
 	levitation_force = 6
 	#your code here
-	loop do
-		levitation_force += 1
+
+	while levitation_force < 10
 		puts "Wingardium Leviosa"
-		if (levitation_force >= 10)
-			break
-		end
+		levitation_force +=1
 	end
+
 
 end
 
