eea97ec2038b8410ef854ef5b7fe27224d9e704f
Janelle <janelle.rosario@gmail.com>
2016-07-11 01:52:10 -0400

    Update until.rb

--- lib/until.rb
+++ lib/until.rb
@@ -1,9 +1,7 @@
-def using_until
+def using_until(phrase)
   levitation_force = 6
-
-
   until levitation_force == 10
-    puts "Wingardium Leviosa"
+    puts "#{phrase}"
     levitation_force += 1
   end
 end
