7cc4caaca8ff85ce283326380ae5340590af1acb
Carmen Vivas <selain27@gmail.com>
2016-07-13 10:34:41 -0400

    I had to change my while

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,11 @@
-def using_while
+def using_while(phrase = "Wingardium Leviosa")
 	levitation_force = 6
 	
-	#your code here
+	begin
+    puts "#{phrase}"
+    levitation_force = levitation_force + 1
+  end while levitation_force < 10
 end
+
 
 
