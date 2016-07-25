c7c8772b7c2bcf3031038e0f88d6c047f34a1626
Sol Jin <soljin01@gmail.com>
2016-07-25 06:18:18 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,16 @@
-def using_until
-        levitation_force = 6
+#def using_until
+#        levitation_force = 6
         #your code here
-    
-end
 
+#end
+
+def using_until
+	counter = 6
+	loop do
+  	counter += 1
+  	puts s = "Wingardium Leviosa"
+  	if counter == 10
+    	break
+  	end
+	end
+end
